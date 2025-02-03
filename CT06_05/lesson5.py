ddbcb3c68537c5d3ed3150ce8cce09b0c091ef4e
Computhink-CS120 <cs120@computhink.com.sg>
2025-02-03 09:49:04 +0000

    Feb 3, 2025, 9:49 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -168,7 +168,7 @@ space=(" ")
 # print(fname+space+lname)
 age=input("what is your age?")
 name=input("wat is your name?")
-print("user de"+space+name+"")
+print("user de"+space+name+"is"+space+age)
 
 
 
