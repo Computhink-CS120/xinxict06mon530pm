f25aba2ff11f578acd32647090dea4003e391098
Computhink-CS120 <cs120@computhink.com.sg>
2025-02-17 10:17:08 +0000

    Feb 17, 2025, 10:17 AM

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -344,7 +344,7 @@ int(t=0)
 b=int(input("how many students do you have?"))
 for q in range(b):
     score=int(input("how much does this sutudent have?"))
-    sumscore=sumscore+score
+    t =sumscore+score
 print ("the average score is "+sumscore/b)
 
 
